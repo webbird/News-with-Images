a1a550a35546f9f335c2017f76d0d2853339594a
Martin Hecht <mrbaseman@gmx.de>
2019-04-25 23:07:24 +0200

    fix for the titles in the overview (use post titles again instead of group titles (which are available as GROUP_TITLE by a later query)

--- view.php
+++ view.php
@@ -154,7 +154,7 @@ if (!defined('POST_ID') or !is_numeric(POST_ID)) {
     }
     
     // Query posts (for this page)
-    $query_posts = $database->query("SELECT p.*,g.active,g.title FROM `".TABLE_PREFIX."mod_news_img_posts` AS p
+    $query_posts = $database->query("SELECT p.*,g.active FROM `".TABLE_PREFIX."mod_news_img_posts` AS p
         LEFT JOIN `".TABLE_PREFIX."mod_news_img_groups` AS g ON (g.group_id = p.group_id OR p.group_id = 0 AND g.group_id = NULL)
         WHERE p.section_id = '$section_id' AND (g.active = 1 OR p.group_id = 0) AND p.active = 1 AND p.title != ''$query_extra
         AND (p.published_when = 0 OR p.published_when <= $t) AND (p.published_until = 0 OR p.published_until >= $t)
